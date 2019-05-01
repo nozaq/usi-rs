43da655ff3d1f3188eaad35781744db7a6a8e713
Travis CI User <travis@example.org>
2019-05-01 15:15:00 +0000

    Documentation for nozaq/usi-rs@4f4cc95

---
+++ implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["usi"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"usi/enum.Error.html\" title=\"enum usi::Error\">Error</a>",synthetic:false,types:["usi::error::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["either"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["itertools"] = {"name":"","dirs":[{"name":"adaptors","dirs":[],"files":["mod.rs","multi_product.rs"]}],"files":["combinations.rs","concat_impl.rs","cons_tuples_impl.rs","diff.rs","either_or_both.rs","format.rs","free.rs","group_map.rs","groupbylazy.rs","impl_macros.rs","intersperse.rs","kmerge_impl.rs","lib.rs","merge_join.rs","minmax.rs","multipeek_impl.rs","pad_tail.rs","peeking_take_while.rs","process_results_impl.rs","put_back_n_impl.rs","rciter_impl.rs","repeatn.rs","size_hint.rs","sources.rs","tee.rs","tuple_impl.rs","unique_impl.rs","with_position.rs","zip_eq_impl.rs","zip_longest.rs","ziptuple.rs"]};
+sourcesIndex["usi"] = {"name":"","dirs":[{"name":"engine","dirs":[],"files":["command.rs","mod.rs","parser.rs"]}],"files":["error.rs","gui.rs","lib.rs"]};
+createSourceSidebar();
